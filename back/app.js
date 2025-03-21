3cf256cf256bcb69a68174671017a637bbe830f9
Sarifor <iread1q84@gmail.com>
2025-03-21 15:56:37 +0900

    Refactor: Remove unnecessary comments and commented-out code

--- back/app.js
+++ back/app.js
@@ -29,10 +29,6 @@ app.use(morgan('dev'));
 // A. Express에서 POST 요청의 body 데이터를 읽으려면 express.json() 미들웨어를 추가해야 해 (ChatGPT)
 app.use(express.json());
 
-// Q. 브라우저가 서버로부터 받은 쿠키를 저장하지 못해 (개발자 도구 -> Application -> Cookies -> 저장된 값 없음)
-// A. domain 설정을 삭제하면 브라우저가 현재 서버 도메인 기준으로 쿠키를 정상적으로 저장할 수 있어! (ChatGPT)
-// Q. 서버에서 보내준 쿠키를 브라우저에 저장할 수 있었어. 근데 문제는 브라우저 새로고침을 하면 모처럼 저장한 쿠키가 사라져버려
-// A. SameSite=Lax로 설정하면 서드파티 요청이 아닌 경우에는 쿠키가 항상 전송되므로, 새로고침 시에도 쿠키가 유지 (ChatGPT)
 app.use(session({
   saveUninitialized: false,
   resave: false,
@@ -40,9 +36,7 @@ app.use(session({
   cookie: {
     httpOnly: true,
     secure: false,
-    // domain: undefined,
     maxAge: 1000 * 60 * 60 * 24,
-    // sameSite: "none",
     sameSite: "lax"
   }
 }));
