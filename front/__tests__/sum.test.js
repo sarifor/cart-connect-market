3446d9d228bd9543545497c1c5286bc749d0a861
Sarifor <iread1q84@gmail.com>
2025-09-11 17:23:24 +0900

    Chore: Add test coverage script and move a test file
    - Add npm script for jest test coverage
    - Move a test file to __tests__ directory

--- front/__tests__/sum.test.js
+++ front/__tests__/sum.test.js
@@ -1,6 +1,6 @@
 // import { jest } from '@jest/globals';
 // const sum = require('./sum.js');
-import sum from './sum.js';
+import sum from '../src/sum.js';
 
 test('1 + 1 = 2', () => {
   expect(sum(1, 1)).toBe(2);
